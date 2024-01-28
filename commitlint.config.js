e411eddc586a51ec7d98bc47384f72a41cb761c8
JonyPlo <joonyyplo@gmail.com>
2024-01-28 03:19:33 -0300

    chore: :wrench: configure commitlint file

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'body-max-line-length': [2, 'always', 10], // Por defecto, el limite de caracteres en el cuerpo del commit es de 100 caracteres, agregar esta regla o quitarla segun sea necesario
+    'header-max-length': [2, 'always', 100], // Por defecto, el limite del titulo del mensaje es de 100, caracteres, modificar este arreglo si es necesario
+    'body-max-line-length': [2, 'always', 100], // Por defecto, el limite de caracteres en el cuerpo del commit es de 100 caracteres, agregar esta regla o quitarla segun sea necesario
   },
-};
+}
