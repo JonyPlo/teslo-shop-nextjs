6f20c6e19ed8287f63ba7a168b4fa3494f95979e
JonyPlo <joonyyplo@gmail.com>
2024-01-29 22:38:06 -0300

    feat(shop): add title component in the shop

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,12 +1,10 @@
+import { Title } from '@/components'
 import { titleFont } from '@/config/fonts'
 
 export default function Home() {
   return (
-    <main className=''>
-      <h1>Hola mundo</h1>
-      {/* De esta forma aplicamos una fuente diferente a una etiqueta, esta fuente aplica tambien para todas la etiquetas que esten dentro de ella */}
-      <h1 className={`${titleFont.className} font-bold`}>Hola mundo</h1>
-      <h1 className={titleFont.className}>Hola mundo</h1>
-    </main>
+    <>
+      <Title title='Shop' subtitle='All products' className='mb-2' />
+    </>
   )
 }
