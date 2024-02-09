b4b279381d7a568075433f237fc88eadb243f9d2
JonyPlo <joonyyplo@gmail.com>
2024-02-09 09:43:30 -0300

    feat(product): :sparkles: add slideshow to show product images

--- src/app/(shop)/product/[slug]/page.tsx
+++ src/app/(shop)/product/[slug]/page.tsx
@@ -2,7 +2,12 @@ import { notFound } from 'next/navigation'
 
 import { initialData } from '@/seed/seed'
 import { titleFont } from '@/config/fonts'
-import { QuantitySelector, SizeSelector } from '@/components'
+import {
+  ProductMobileSlideshow,
+  ProductSlideshow,
+  QuantitySelector,
+  SizeSelector,
+} from '@/components'
 
 const seedProducts = initialData.products
 
@@ -25,23 +30,34 @@ export default function ProductPage({ params }: Props) {
     <div className='mt-5 mb-20 grid md:grid-cols-3 gap-3'>
       {/* Slideshow */}
       <div className='md:col-span-2'>
-        <h1>Hello</h1>
+        {/* Desktop Slideshow */}
+        <ProductSlideshow
+          title={product.title}
+          images={product.images}
+          className='hidden md:block'
+        />
+        {/* Mobile Slideshow */}
+        <ProductMobileSlideshow
+          title={product.title}
+          images={product.images}
+          className='block md:hidden'
+        />
       </div>
-
       {/* Details */}
       <div className='px-5'>
         <h1 className={`${titleFont.className} antialiased font-bold text-xl`}>
           {product.title}
         </h1>
         <p className='text-lg mb-5'>${product.price}</p>
-
         {/* Sizes selector */}
-        <SizeSelector selectedSize={ product.sizes[0]} availableSizes={product.sizes}/>
+        <SizeSelector
+          selectedSize={product.sizes[0]}
+          availableSizes={product.sizes}
+        />
         {/* Quantity selector */}
-<QuantitySelector quantity={2}/>
+        <QuantitySelector quantity={2} />
         {/* Button */}
         <button className='btn-primary my-5'>Add to cart</button>
-
         {/* Description */}
         <h3 className='font-bold text-sm'>Description</h3>
         <p className='font-light'>{product.description}</p>
