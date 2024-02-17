86ca20df88416f7ca1630e855cdafee0b66a94ba
JonyPlo <joonyyplo@gmail.com>
2024-02-17 20:10:22 -0300

    chore(prisma): update seed file

--- src/seed/seed-database.ts
+++ src/seed/seed-database.ts
@@ -3,13 +3,24 @@
 //* Tener en cuenta que para que funcionen las importaciones en node, el package json tiene que ser de tipo modulo, o la otra solución que es la que se implemento aqui, es movernos al directorio seed con el comando `cd src/seed` y dentro de ese directorio ejecutar el comando `npm tsc --init`, este comando creara el archivo tsconfig.json dentro de la carpeta "seed/", y tendra las configuraciones por defecto para que en Typescript funcionen las importaciones
 
 import { initialData } from './seed'
+import prisma from '../lib/prisma'
+
+// De esta forma creamos un seed para una base de datos
 
 const main = async () => {
-  // Esta condicional es importante para que la funcion no se ejecute una vez que el proyecto ya esta en producción
-  if (process.env.NODE_ENV === 'production') return
+  // Borrar registros previos
+  await Promise.all([
+    prisma.productImage.deleteMany(),
+    prisma.product.deleteMany(),
+    prisma.category.deleteMany(),
+  ])
 
-  console.log(initialData)
   console.log('Seed')
 }
 
-main()
+;(() => {
+  // Esta condicional es importante para que la funcion no se ejecute una vez que el proyecto ya esta en producción
+  if (process.env.NODE_ENV === 'production') return
+
+  main()
+})()
