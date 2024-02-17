c43a91f9780b80eaac1c754361d0d95f0827e70c
JonyPlo <joonyyplo@gmail.com>
2024-02-17 16:13:37 -0300

    chore(seed): :seedling: add seed file

---
+++ src/seed/seed-database.ts
@@ -0,0 +1,15 @@
+//! Para ejecutar este archivo vamos a usar el comando de "node src/...", pero el archivo es de Typescript, y node no reconoce este tipado, asi que para poder ejecutar codigo de Typescript en node tenemos que instalar la dependencia "ts-node" como dependencia de desarrollo `pnpm i -D ts-node`, y usaremos ts-node para poder ejecutar el archivo, que en vez de `node src/seed/seed-database.ts` seria `ts-node src/seed/seed-database.ts`
+
+//* Tener en cuenta que para que funcionen las importaciones en node, el package json tiene que ser de tipo modulo, o la otra solución que es la que se implemento aqui, es movernos al directorio seed con el comando `cd src/seed` y dentro de ese directorio ejecutar el comando `npm tsc --init`, este comando creara el archivo tsconfig.json dentro de la carpeta "seed/", y tendra las configuraciones por defecto para que en Typescript funcionen las importaciones
+
+import { initialData } from './seed'
+
+const main = async () => {
+  // Esta condicional es importante para que la funcion no se ejecute una vez que el proyecto ya esta en producción
+  if (process.env.NODE_ENV === 'production') return
+
+  console.log(initialData)
+  console.log('Seed')
+}
+
+main()
