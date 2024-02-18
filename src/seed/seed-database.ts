2d9ca71aa6b81ed2df75ff2fdce88860c9b734a4
JonyPlo <joonyyplo@gmail.com>
2024-02-17 21:22:39 -0300

    chore(seed): :seedling: seed file done

--- src/seed/seed-database.ts
+++ src/seed/seed-database.ts
@@ -5,20 +5,58 @@
 import { initialData } from './seed'
 import prisma from '../lib/prisma'
 
-// De esta forma creamos un seed para una base de datos
-
+// De esta forma creamos un seed para una base de datos SQL con Prisma
 const main = async () => {
   // Borrar registros previos
-  await Promise.all([
-    prisma.productImage.deleteMany(),
-    prisma.product.deleteMany(),
-    prisma.category.deleteMany(),
-  ])
+  await prisma.productImage.deleteMany()
+  await prisma.product.deleteMany()
+  await prisma.category.deleteMany()
 
-  console.log('Seed')
+  const { categories, products } = initialData
+
+  // Insertar Categorias en la db
+  const categoriesData = categories.map((name) => ({
+    name,
+  }))
+
+  await prisma.category.createMany({
+    data: categoriesData,
+  })
+
+  const categoriesDB = await prisma.category.findMany()
+
+  const categoriesMap = categoriesDB.reduce((map, category) => {
+    map[category.name.toLowerCase()] = category.id
+
+    return map
+  }, {} as Record<string, string>)
+
+  products.forEach(async (product) => {
+    const { type, images, ...rest } = product
+
+    // Insertar Productos en la db
+    const dbProduct = await prisma.product.create({
+      data: {
+        ...rest,
+        categoryId: categoriesMap[type],
+      },
+    })
+
+    const imagesData = images.map((image) => ({
+      url: image,
+      productId: dbProduct.id,
+    }))
+
+    // Insertar Imagenes en la db
+    await prisma.productImage.createMany({
+      data: imagesData,
+    })
+  })
+
+  console.log('Seed executed')
 }
 
-;(() => {
+(() => {
   // Esta condicional es importante para que la funcion no se ejecute una vez que el proyecto ya esta en producción
   if (process.env.NODE_ENV === 'production') return
 
