b8b5ed5469f5c09c1f366847711bb58c7a87b8b4
JonyPlo <joonyyplo@gmail.com>
2024-03-25 00:10:34 -0300

    feat(auth): :sparkles: add users seed to introduce these data in database

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -1,11 +1,13 @@
 import NextAuth, { type NextAuthConfig } from 'next-auth'
 import credentials from 'next-auth/providers/credentials'
 import { z } from 'zod'
+import prisma from '@/lib/prisma'
+import bcrypt from 'bcryptjs'
 
 //* Configuracion para inicio de sesion y registro en paginas custom
 
 export const authConfig: NextAuthConfig = {
-  // En la propiedad pages pondremos la ruta a la que accedemos para iniciar secion y para registrarse, por ejemplo localmente el path de la pagina de inicio de sesion es "http://localhost:3000/auth/login" por lo tanto el path que tengo que agregar en la propiedad "signIn" es "/auth/login"
+  // En la propiedad 'pages' pondremos la ruta a la que accedemos para iniciar secion y para registrarse, por ejemplo localmente el path de la pagina de inicio de sesion es "http://localhost:3000/auth/login" por lo tanto el path que tengo que agregar en la propiedad "signIn" es "/auth/login"
   pages: {
     signIn: '/auth/login',
     newUser: '/auth/new-account',
@@ -27,15 +29,25 @@ export const authConfig: NextAuthConfig = {
         // Si las validaciones salen bien y tenemos el objeto entonces desestructuramos el objeto para obtener el email y password
         const { email, password } = parsedCredentials.data
 
-        console.log({ email, password })
+        // Comprobar si el email existe en la db
+        const user = await prisma.user.findUnique({
+          where: { email: email.toLowerCase() },
+        })
 
-        // Buscar el correo
+        if (!user) return null
 
         // Comparar los passwords
+        const passwordMatch = bcrypt.compareSync(password, user.password)
 
-        // Regresar el user
+        if (!passwordMatch) return null
 
-        return null
+        // Si llegamos hasta aqui quiere decir que la persona ya esta autenticada, asi que quitamos el password del objeto 'user' porque ya no lo necesitamos
+        const { password: _, ...rest } = user
+
+        // Por ultimo retornamos el objeto 'rest' que seria el objeto 'user' pero sin el password, y este objeto contiene la informacion necesaria que va a pasar a los diferentes callbacks de NextAuth para obtener la informacion de la persona autenticada en cualquier parte de la aplicacion
+        console.log({ rest })
+
+        return rest
       },
     }),
   ],
