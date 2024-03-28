59d0b7a2854917628cc7d9930e93b00aeee9a5d5
JonyPlo <joonyyplo@gmail.com>
2024-03-28 03:47:48 -0300

    feat(home-page): :lipstick: add fade in-out animation in images of home page

--- src/components/products/product-grid/ProductGridItem.tsx
+++ src/components/products/product-grid/ProductGridItem.tsx
@@ -1,33 +1,36 @@
-'use client'
-
 import type { Product } from '@/interfaces'
 import Image from 'next/image'
 import Link from 'next/link'
-import { useState } from 'react'
 
 interface Props {
   product: Product
 }
 
 export const ProductGridItem = ({ product }: Props) => {
-  const [displayImage, setDisplayImage] = useState(product.images[0])
-
-  const asd = 'sdfd'
-
   return (
-    <div className='fade-in overflow-hidden'>
-      <Link href={`/product/${product.slug}`}>
-        <Image
-          src={`/products/${displayImage}`}
-          alt={`${product.title}`}
-          className='w-full rounded object-cover'
-          width={500}
-          height={500}
-          onMouseEnter={() => setDisplayImage(product.images[1])}
-          onMouseLeave={() => setDisplayImage(product.images[0])}
-          priority
-        />
-      </Link>
+    <div className='fade-in'>
+      <div className='overflow-hidden'>
+        <Link href={`/product/${product.slug}`} className='relative'>
+          {/* Front image */}
+          <Image
+            src={`/products/${product.images[0]}`}
+            alt={`${product.title}`}
+            className='absolute top-0 rounded duration-150 ease-in-out hover:opacity-0'
+            width={500}
+            height={500}
+            priority
+          />
+          {/* Back image */}
+          <Image
+            src={`/products/${product.images[1]}`}
+            alt={`${product.title}`}
+            className='rounded-md'
+            width={500}
+            height={500}
+            priority
+          />
+        </Link>
+      </div>
 
       <div className='flex flex-col py-4'>
         <Link className='hover:text-blue-600' href={`/product/${product.slug}`}>
