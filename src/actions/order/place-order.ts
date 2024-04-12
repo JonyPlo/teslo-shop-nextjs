97004a031dd50293ccfac2ee6492fcf0ed753ce9
JonyPlo <joonyyplo@gmail.com>
2024-04-12 04:00:37 -0300

    feat(checkout-page): :sparkles: start creating order transactions with prisma

--- src/actions/order/place-order.ts
+++ src/actions/order/place-order.ts
@@ -38,7 +38,7 @@ export const placeOrder = async (
       },
     })
 
-    // Calcular los montos
+    // Calcular la cantidad de items a comprar
     const itemsInOrder = productIds.reduce(
       (acc, product) => acc + product.quantity,
       0
@@ -64,6 +64,45 @@ export const placeOrder = async (
       },
       { subTotal: 0, tax: 0, total: 0 }
     )
+
+    // Crear la transaccion de base de datos
+    //* Aqui realizamos una transaccion con prisma, y en lugar de trabajar directamente con prisma usaremos 'tx' (tx hace referencia a la palabra 'transaction') que tiene acceso a todos los mismos objetos que tiene prisma, y a medida que se va avanzando tx va almacenando en memoria todos los pasos sin afectar a la base de datos, y solo si se completaron todos de manera exitosa, entonces se retornaran todos los pasos y ahora si estos afectaran a la base de datos, pero si en el medio del proceso, en algun paso hubo un error, como por ejemplo el stock de un producto en la base de datos bajo a 0, entonces lanzamos un error y tx realizara un rollback para cancelar lo que se hizo en los pasos anteriores por nosotros
+    //! IMPORTANTE: dentro del metodo $transaction() no podemos utilizar prisma de la siguiente forma 'prisma.order.find()...' si no que vamos a usar el elemento 'tx', por ejemplo 'tx.order.find()...'
+    const prismaTx = await prisma.$transaction(async (tx) => {
+      // 1. Actualizar el stock de los productos
+
+      // 2. Crear la orden -  Encabezado y Detalle
+      const order = await tx.order.create({
+        data: {
+          userId,
+          itemsInOrder,
+          subTotal,
+          tax,
+          total,
+
+          // Como la tabla 'Order' tiene una relacion con la tabla 'OrderItem', entonces aqui mismo podemos hacer la incersion de los items en la tabla OrderItem
+          //* Un OrderItem requiere una propiedad llamada 'orderId', pero en este caso prisma se encargara de crear esa propiedad por nosotros, ya que primero crea la orden, y una vez creada toma el id que le creo la base de datos para esa orden y lo guarda dentro de la propiedad orderId en cada item de la tabla OrderItem
+          OrderItem: {
+            createMany: {
+              data: productIds.map((cartProd) => ({
+                quantity: cartProd.quantity,
+                size: cartProd.size,
+                productId: cartProd.productId,
+                price:
+                  dbProducts.find((dbProd) => dbProd.id === cartProd.productId)
+                    ?.price ?? 0,
+              })),
+            },
+          },
+        },
+      })
+
+      // 3. Crear la direccion de la orden
+
+      return {
+        order,
+      }
+    })
   } catch (error) {
     logger.error('Error to save Order', error)
   }
