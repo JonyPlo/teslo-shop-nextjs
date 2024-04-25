2392b92519fde2cf04b3096e95724824e7dbe11d
JonyPlo <joonyyplo@gmail.com>
2024-04-25 03:47:32 -0300

    fix: :adhesive_bandage: fix warnings and errors in production build

--- src/app/auth/new-account/page.tsx
+++ src/app/auth/new-account/page.tsx
@@ -1,11 +1,14 @@
 import { titleFont } from '@/config/fonts'
 import { RegisterForm } from './ui/RegisterForm'
+import { Suspense } from 'react'
 
 export default function RegisterPage() {
   return (
     <div className='flex min-h-screen flex-col justify-center'>
       <h1 className={`${titleFont.className} mb-5 text-4xl`}>Register</h1>
-      <RegisterForm />
+      <Suspense>
+        <RegisterForm />
+      </Suspense>
     </div>
   )
 }
