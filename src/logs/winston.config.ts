508a4ddc494a64800550adeb84f1740e7d89f2d5
JonyPlo <joonyyplo@gmail.com>
2024-04-25 18:36:48 -0300

    refactor: delete logs in file directory

--- src/logs/winston.config.ts
+++ src/logs/winston.config.ts
@@ -1,21 +1,7 @@
 import winston from 'winston'
-import 'winston-daily-rotate-file'
 
-const { format, transports, createLogger } = winston
+const { transports, createLogger } = winston
 const { combine, timestamp, printf, colorize, align } = winston.format
-
-// Level filters
-const combineFilter = format((info) => {
-  return info.level === 'info' || info.level === 'error' ? info : false
-})
-
-const errorFilter = format((info) => {
-  return info.level === 'error' ? info : false
-})
-
-const infoFilter = format((info) => {
-  return info.level === 'info' ? info : false
-})
 
 // Global formats
 const globalFormats = [
@@ -26,48 +12,12 @@ const globalFormats = [
   printf((info) => `\n[${info.timestamp}]\n${info.level}: ${info.message}`),
 ]
 
-// File rotates
-const fileRotateTransport = (
-  filename: string,
-  datePattern: string,
-  maxFiles: string,
-  filter: winston.Logform.Format
-) => {
-  return new transports.DailyRotateFile({
-    filename,
-    datePattern,
-    maxFiles,
-    format: combine(filter, ...globalFormats),
-  })
-}
-
 // Logger
 export const logger = createLogger({
   level: process.env.LOG_LEVEL || 'info',
   transports: [
     new transports.Console({
       format: combine(colorize({ all: true }), ...globalFormats),
-    }),
-    // Combined log File
-    fileRotateTransport(
-      'src/logs/log-files/combined-logs/combined-logs-%DATE%.log',
-      'YYYY-MM-DD',
-      '2d',
-      combineFilter()
-    ),
-    // Error log File
-    fileRotateTransport(
-      'src/logs/log-files/error-logs/error-logs-%DATE%.log',
-      'YYYY-MM-DD',
-      '2d',
-      errorFilter()
-    ),
-    // Info log File
-    fileRotateTransport(
-      'src/logs/log-files/info-logs/info-logs-%DATE%.log',
-      'YYYY-MM-DD',
-      '2',
-      infoFilter()
-    ),
+    })
   ],
 })
